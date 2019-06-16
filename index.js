6e70a442cf9da29e4bc086b0d8df573e34e16e46
Kevin Damian Ta <kevindamianta@gmail.com>
2019-06-16 15:45:15 -0700

    Base Files
    Made initial commit; created HTML, CSS, and Javascript files needed.

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function getDogImages() {
+
+}
+
+function displayDoggos() {
+
+}
+
+function formSubmission() {
+
+}
+
+function renderDoggos() {
+	console.log('App is loaded. Waiting for doggos!');
+	formSubmission();
+}
+
+renderDoggos();
